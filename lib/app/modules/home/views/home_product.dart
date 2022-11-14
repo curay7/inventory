14557ffe0669411ed2c7f37f699c91cdb8732171
lone <georgecuray7@gmail.com>
2022-11-14 22:07:41 +0800

    added util for flutter flow

--- lib/app/modules/home/views/home_product.dart
+++ lib/app/modules/home/views/home_product.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:responsive_table/responsive_table.dart';
 
+import '../../../data/flutter_flow/flutter_flow_theme.dart';
+
 class HomeProduct extends StatefulWidget {
   const HomeProduct({super.key});
 
@@ -434,31 +436,347 @@ class _DropDownContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> _children = data.entries.map<Widget>((entry) {
-      Widget w = Row(
-        children: [
-          Text(entry.key.toString()),
-          SizedBox(
-            width: 170,
-          ),
-          Text(entry.value.toString()),
-        ],
-      );
-      return w;
-    }).toList();
+    List<Widget> _children = data.entries.map<Widget>(
+      (entry) {
+        Widget w = Row(
+          children: [
+            Text(entry.key.toString()),
+            SizedBox(
+              width: 170,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [Text("Row 2"), Text("Test")],
+            )
+          ],
+        );
+        return w;
+      },
+    ).toList();
+
+    print(data[1]);
+    print(data[1]);
 
     return Container(
-      // height: 100,
-      child: Column(
-        /// children: [
-        ///   Expanded(
-        ///       child: Container(
-        ///     color: Colors.red,
-        ///     height: 50,
-        ///   )),
-        /// ],
-        children: _children,
-      ),
-    );
+        child: // Generated code for this Column Widget...
+            Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Row(
+          children: [
+            Container(
+              height: 220,
+              width: 220,
+              padding: EdgeInsets.all(20),
+              child: Stack(
+                alignment: AlignmentDirectional(-0.95, -0.7),
+                children: [
+                  Align(
+                      alignment: AlignmentDirectional(0, 0),
+                      child: Image.network(
+                        'https://picsum.photos/seed/921/600',
+                        fit: BoxFit.cover,
+                      )
+
+                      // Image.asset(
+                      //   'assets/images/ECED2980-86B9-4713-9459-9EE622B04DD8.JPG',
+                      //   width: MediaQuery.of(context).size.width,
+                      //   height: 240,
+                      //   fit: BoxFit.cover,
+                      // ),
+                      ),
+                  // Align(
+                  //   alignment: AlignmentDirectional(-0.95, -0.55),
+                  //   child: InkWell(
+                  //     onTap: () async {
+                  //       //context.pop();
+                  //     },
+                  //     child: Card(
+                  //       clipBehavior: Clip.antiAliasWithSaveLayer,
+                  //       color: Color(0xFFF5F5F5),
+                  //       elevation: 3,
+                  //       shape: RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.circular(100),
+                  //       ),
+                  //       child: Padding(
+                  //         padding:
+                  //             EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
+                  //         child: Icon(
+                  //           Icons.arrow_back_rounded,
+                  //           color: FlutterFlowTheme.of(context).primaryColor,
+                  //           size: 24,
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(20, 16, 20, 0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Coffee Name',
+                            style: FlutterFlowTheme.of(context).bodyText2,
+                          ),
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                            child: Text(
+                              'Colombian (San Juan)',
+                              textAlign: TextAlign.start,
+                              style: FlutterFlowTheme.of(context).title2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(20, 8, 20, 0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Coffee (g)',
+                            style: FlutterFlowTheme.of(context).bodyText2,
+                          ),
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                            child: Text(
+                              '32g',
+                              textAlign: TextAlign.start,
+                              style: FlutterFlowTheme.of(context).subtitle1,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(32, 0, 0, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Water (g)',
+                              style: FlutterFlowTheme.of(context).bodyText2,
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                              child: Text(
+                                '456g',
+                                textAlign: TextAlign.start,
+                                style: FlutterFlowTheme.of(context).subtitle1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(32, 0, 0, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Ratio',
+                              style: FlutterFlowTheme.of(context).bodyText2,
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                              child: Text(
+                                '1/17',
+                                textAlign: TextAlign.start,
+                                style: FlutterFlowTheme.of(context).subtitle1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(20, 16, 20, 0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Brew Time',
+                      style: FlutterFlowTheme.of(context).bodyText2,
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                      child: Text(
+                        '4:30',
+                        textAlign: TextAlign.start,
+                        style: FlutterFlowTheme.of(context).subtitle2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Rating',
+                      style: FlutterFlowTheme.of(context).bodyText2,
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                          child: Text(
+                            '6',
+                            textAlign: TextAlign.start,
+                            style: FlutterFlowTheme.of(context).subtitle2,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                          child: Icon(
+                            Icons.star_rate_rounded,
+                            color: FlutterFlowTheme.of(context).primaryColor,
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(20, 16, 20, 0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Grind Size',
+                      style: FlutterFlowTheme.of(context).bodyText2,
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                      child: Text(
+                        '6.5',
+                        textAlign: TextAlign.start,
+                        style: FlutterFlowTheme.of(context).subtitle2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Grinder Used',
+                      style: FlutterFlowTheme.of(context).bodyText2,
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                      child: Text(
+                        'Ode Grinder',
+                        textAlign: TextAlign.start,
+                        style: FlutterFlowTheme.of(context).subtitle2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(20, 16, 20, 0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Coffee Notes',
+                      style: FlutterFlowTheme.of(context).bodyText2,
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                      child: Text(
+                        'The coffee was decent with notes of caramel, lemongrass, slight hit of berry.\n',
+                        textAlign: TextAlign.start,
+                        style: FlutterFlowTheme.of(context).subtitle2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Divider(),
+      ],
+    )
+
+        // height: 100,
+        // child: Column(
+        //   /// children: [
+        //   ///   Expanded(
+        //   ///       child: Container(
+        //   ///     color: Colors.red,
+        //   ///     height: 50,
+        //   ///   )),
+        //   /// ],
+        //   children: _children,
+        // ),
+        );
   }
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <sidebar_bigeagle/side_bar.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   SideBarRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SideBar"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
